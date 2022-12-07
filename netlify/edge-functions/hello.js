5c1815e68b70537e0ae04928bac253ea1f6c7990
mknori9 <mknori9@gmail.com>
2022-12-07 17:13:02 +0530

    test

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,5 @@
 export default async(request, context) => {
-    context.log("Calling hello.js Function"); 
+    context.log("Calling hello.js Function Test"); 
     return new Response("Hello World form the Edge!", {
         headers: {
             "content-type": "text/html"
